7de5aa2dc28117916fe4edd80f27e359b18124cc
zeno <zeno.bcit@gmail.com>
2024-01-06 21:49:29 -0800

    Edit Layout v1.0.2

--- components/Login/loginIndex.js
+++ components/Login/loginIndex.js
@@ -6,7 +6,7 @@ export default function Login() {
     <div className={styles.loginFormContainer}>
       <div className={styles.loginFormTitle}>
         <h1>Happening now</h1>
-        <h2>Join today</h2>
+        <h2>Join today.</h2>
       </div>
       <div className={styles.loginFormButton}>
         <button className={styles.loginButton}>
@@ -16,10 +16,14 @@ export default function Login() {
           <svg viewBox="0 0 24 24" aria-hidden="true" className={styles.appleIcon}><g><path d="M16.365 1.43c0 1.14-.493 2.27-1.177 3.08-.744.9-1.99 1.57-2.987 1.57-.12 0-.23-.02-.3-.03-.01-.06-.04-.22-.04-.39 0-1.15.572-2.27 1.206-2.98.804-.94 2.142-1.64 3.248-1.68.03.13.05.28.05.43zm4.565 15.71c-.03.07-.463 1.58-1.518 3.12-.945 1.34-1.94 2.71-3.43 2.71-1.517 0-1.9-.88-3.63-.88-1.698 0-2.302.91-3.67.91-1.377 0-2.332-1.26-3.428-2.8-1.287-1.82-2.323-4.63-2.323-7.28 0-4.28 2.797-6.55 5.552-6.55 1.448 0 2.675.95 3.6.95.865 0 2.222-1.01 3.902-1.01.613 0 2.886.06 4.374 2.19-.13.09-2.383 1.37-2.383 4.19 0 3.26 2.854 4.42 2.955 4.45z"></path></g></svg><span>Sign up with Apple</span>
         </button>
         <div className={styles.separateItem}>
-          <span>OR</span>
+          <span>or</span>
         </div>
-        <button className={styles.loginButton}>Create account</button>
+        <Link href="https://twitter.com/i/flow/signup" className={styles.createAccButton}>Create account</Link>
         <p className={styles.description}>By signing up, you agree to the <span><Link href="https://twitter.com/tos">Terms of Service</Link></span> and <span><Link href="https://twitter.com/privacy">Privacy Policy</Link></span>, including <span><Link href="https://help.twitter.com/rules-and-policies/twitter-cookies">Cookie Use</Link></span>.</p>
+        <div className={styles.signInContainer}>
+          <h3>Already have an account?</h3>
+          <Link href="https://twitter.com/login" className={styles.signInButton}>Sign in</Link>
+        </div>
       </div>
     </div>
   )
